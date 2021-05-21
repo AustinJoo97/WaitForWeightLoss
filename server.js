e0b78d9ca1df0b97c6daf56f7ca160012c47a28a
AustinJoo97 <austinjoo97@gmail.com>
2021-05-20 20:51:02 -0400

    added dotenv to server.js; created config dir with connection.js to connect to database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
