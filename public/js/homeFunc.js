b0ef0b37707ffef69dd4e00d603a6b13b3b5eaef
AustinJoo97 <austinjoo97@gmail.com>
2021-05-27 20:36:01 -0400

    functionality working successfully where clicking corresponding buttons will swap the user between the overview (home) page and indepth page; all data transferring properly from api route calls to handlebar; need to properly apply data to the script at dashboard handlebar to render chart

--- public/js/homeFunc.js
+++ public/js/homeFunc.js
@@ -1,26 +1,3 @@
-// All of these functions are meant to be run when the basic homepage is serving as the user's current DOM
-
-const getBasicView = async () => {
-    const basicWeightData = await fetch('/');
-
-    if(basicWeightData.ok){
-        return;
-    } else {
-        alert(response.statusText)
-    }
-}
-
-// Get chart and all weight info for graphed weights over time and in-depth profile; this should be triggered when the user is switching to in-depth mode
-const getInDepthView = async () => {
-    const allWeightData = await fetch('api/weight/');
-
-    if(allWeightData.ok){
-        return;
-    } else {
-        alert(response.statusText)
-    }
-}
-
 const newWeightEntry = async(event) => {
     event.preventDefault();
 
@@ -63,8 +40,6 @@ const updateGoalWeight = async (event) => {
         alert(newWeightEntry.statusText);
     }
 }
-
-// document.getElementById('in_depth_button').addEventListener('onclick', getInDepthView);
 
 document.querySelector('#currentWeightForm').addEventListener('submit', newWeightEntry);
 document.querySelector('#goalWeightForm').addEventListener('submit', updateGoalWeight);
