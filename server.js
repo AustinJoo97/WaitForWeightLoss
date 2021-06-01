d9f2832e35ad6e13b94fbf24a662a0d145b941e9
Daisha Marrow <marrowdaisha@gmail.com>
2021-05-31 22:57:01 -0400

    updated styling

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 require('dotenv').config();
 
+
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
